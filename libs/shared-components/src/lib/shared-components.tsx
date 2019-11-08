d1a5bc79b4f37bfe7a0cf83a67700b59453b0bc1
Muhamad Risman <real.risman@gmail.com>
2019-11-08 13:11:52 +0700

    Init lib shared-components

---
+++ libs/shared-components/src/lib/shared-components.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './shared-components.css';
+
+/* eslint-disable-next-line */
+export interface SharedComponentsProps {}
+
+export const SharedComponents = (props: SharedComponentsProps) => {
+  return (
+    <div>
+      <h1>Welcome to shared-components component!</h1>
+    </div>
+  );
+};
+
+export default SharedComponents;
